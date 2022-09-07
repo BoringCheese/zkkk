dce286ee27f80b24c04f0f10aea45b5093e92011
BoringCheese <78197512+BoringCheese@users.noreply.github.com>
2022-09-07 09:54:33 +0800

    组织结构2
    组织结构2

--- structure.py
+++ structure.py
@@ -29,5 +29,36 @@ r = range(1, 10)  # 从1到10步长为1,不包括10
 print(list(r))
 r = range(1, 10, 2)  # 从1到10步长为2,不包括10
 print(list(r))
-#判断指定整数在序列中是否存在用 in, not in
+# 判断指定整数在序列中是否存在用 in, not in
 print(10 in r)
+
+# while
+# else在下面表示循环正常结束时执行
+sum1 = 0
+a = 2
+while a < 101:
+    sum1 += a
+    a += 2
+else:
+    print(sum1)
+
+# for-in-
+# for 自定义变量 in 可迭代变量 :
+# else在下面表示循环正常结束时执行
+for item in 'Python':  # 依次取出里面的值给item
+    print(item)
+for i in range(10):
+    print(i)
+for _ in range(5):  # 没有自定义变量用_代替
+    print('没有自定义变量用_代替')
+for i in range(100, 1000, 1):
+    a = i % 10
+    b = i // 10 % 10
+    c = i // 100
+    if i == a * a * a + b * b * b + c * c * c:
+        print(i)
+else:
+    print('循环结束')
+
+# break语句，用于结束循环结构，通常和if分支结构一起使用
+# continue语句，用于结束当前循环，进入下一次循环。通常和if分支结构一起使用
